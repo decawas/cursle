20ebf6e3e3bb8f75d8dae932296c7dc687ff89a0
Ekno <88718875+decawas@users.noreply.github.com>
2022-06-24 01:29:12 +0100

    fixed numbers of tries over 6 not working

--- cursle.py
+++ cursle.py
@@ -7,7 +7,7 @@ from re import search as res
 class globals:
     word = ""
     words = ""
-    guess = ["", 0]
+    guess = ""
     guesses = []
 
 class renderModeCurses:
@@ -28,85 +28,75 @@ class renderModeCurses:
         stdscr.keypad(True)
 
         while True:
-            try:
-                try:
-                    renderModeCurses.render(stdscr)
-                except:
-                    pass
-                if globals.guesses[-1] != "":
-                    curses.endwin()
-                    print("the word was {0}".format(globals.word))
-                    import sys
-                    sys.exit()
+            renderModeCurses.render(stdscr)
 
-                x = str(stdscr.getkey())
+            key = str(stdscr.getkey()).lower()
             
-                if x == "\x0A" or x == "KEY_ENTER":
-                    if len(globals.guess[0]) != 5 or res(globals.guess[0] ,globals.words) == None:
-                        pass
-                    else:
-                        globals.guesses[globals.guess[1]] = globals.guess[0].lower()
-                        globals.guess[0] = ""
-                        globals.guess[1] += 1
-                elif x == "\x7f" or x == "KEY_BACKSPACE":
-                    globals.guess[0] = globals.guess[0][:-1]
-                elif len(globals.guess[0]) >= 5:
-                    pass
-                elif res("[a-z:]", x.lower()) != None and x != "KEY_RESIZE":
-                    globals.guess[0] += x
-            except KeyboardInterrupt:
-                curses.endwin()
-                print("the word was {0}".format(globals.word))
-                import sys
-                sys.exit()
+            if key == "\n" or key == "key_enter":
+                if len(globals.guess) != 5 or res(globals.guess ,globals.words) == None: pass
+                else:
+                    globals.guesses.append(globals.guess)
+                    globals.guess = ""
+            elif key == "key_backspace": globals.guess = globals.guess[:-1]
+            elif len(globals.guess) >= 5: pass
+            elif res("[a-z]", key) != None:
+                globals.guess += key
+            fi = open("a.txt", "a")
+            fi.write(globals.guess)
+            fi.write("\n")
+            fi.write(str(key))
+            fi.close()
         
-
     def render(stdscr):
         stdscr.bkgd(" ", curses.color_pair(1))
         stdscr.refresh()
         stdscr.clear()
+        fi = open("a.txt", "a")
+        fi.write(globals.guess)
+        fi.write("\n")
+        fi.close()
+        i = 0
         for i in range(args.tries):
-            if len(globals.guesses[i]) == 5:
-                a = ""
-                b = ""
-                c = ""
-                for j0 in range(5):
-                    if globals.word[j0] == globals.guesses[i][j0]: stdscr.addstr("┌─┐ ", curses.color_pair(2))
-                    elif globals.word[j0] != globals.guesses[i][j0] and res(globals.guesses[i][j0], globals.word) != None: stdscr.addstr("┌─┐ ", curses.color_pair(3))
-                    elif globals.word[j0] != globals.guesses[i][j0] and res(globals.guesses[i][j0], globals.word) == None: stdscr.addstr("┌─┐ ", curses.color_pair(4))
-                stdscr.addstr("\n")
-                for j1 in range(5):
-                    if globals.word[j1] == globals.guesses[i][j1]: stdscr.addstr("│" + str(globals.guesses[i][j1]).upper() + "│ ", curses.color_pair(2))
-                    elif globals.word[j1] != globals.guesses[i][j1] and res(globals.guesses[i][j1], globals.word) != None: stdscr.addstr("│" + str(globals.guesses[i][j1]).upper() + "│ ", curses.color_pair(3))
-                    elif globals.word[j1] != globals.guesses[i][j1] and res(globals.guesses[i][j1], globals.word) == None: stdscr.addstr("│" + str(globals.guesses[i][j1]).upper() + "│ ", curses.color_pair(4))
-                stdscr.addstr("\n")
-                for j2 in range(5):
-                    if globals.word[j2] == globals.guesses[i][j2]: stdscr.addstr("└─┘ ", curses.color_pair(2))
-                    elif globals.word[j2] != globals.guesses[i][j2] and res(globals.guesses[i][j2], globals.word) != None: stdscr.addstr("└─┘ ", curses.color_pair(3))
-                    elif globals.word[j2] != globals.guesses[i][j2] and res(globals.guesses[i][j2], globals.word) == None: stdscr.addstr("└─┘ ", curses.color_pair(4))
-                stdscr.addstr("\n")
+            try:
+                if len(globals.guesses[i]) == 5:
+                    for j0 in range(5):
+                        if globals.word[j0] == globals.guesses[i][j0]: stdscr.addstr("┌─┐ ", curses.color_pair(2))
+                        elif globals.word[j0] != globals.guesses[i][j0] and res(globals.guesses[i][j0], globals.word) != None: stdscr.addstr("┌─┐ ", curses.color_pair(3))
+                        elif globals.word[j0] != globals.guesses[i][j0] and res(globals.guesses[i][j0], globals.word) == None: stdscr.addstr("┌─┐ ", curses.color_pair(4))
+                    stdscr.addstr("\n")
+                    for j1 in range(5):
+                        if globals.word[j1] == globals.guesses[i][j1]: stdscr.addstr("│" + str(globals.guesses[i][j1]).upper() + "│ ", curses.color_pair(2))
+                        elif globals.word[j1] != globals.guesses[i][j1] and res(globals.guesses[i][j1], globals.word) != None: stdscr.addstr("│" + str(globals.guesses[i][j1]).upper() + "│ ", curses.color_pair(3))
+                        elif globals.word[j1] != globals.guesses[i][j1] and res(globals.guesses[i][j1], globals.word) == None: stdscr.addstr("│" + str(globals.guesses[i][j1]).upper() + "│ ", curses.color_pair(4))
+                    stdscr.addstr("\n")
+                    for j2 in range(5):
+                        if globals.word[j2] == globals.guesses[i][j2]: stdscr.addstr("└─┘ ", curses.color_pair(2))
+                        elif globals.word[j2] != globals.guesses[i][j2] and res(globals.guesses[i][j2], globals.word) != None: stdscr.addstr("└─┘ ", curses.color_pair(3))
+                        elif globals.word[j2] != globals.guesses[i][j2] and res(globals.guesses[i][j2], globals.word) == None: stdscr.addstr("└─┘ ", curses.color_pair(4))
+                    stdscr.addstr("\n")
+            except:
+                if globals.guess != "" and len(globals.guesses) == i:
+                    a = ""
+                    b = ""
+                    c = ""
+                    for j in range(5):
+                        a += "┌─┐ "
+                        try:
+                            b += " " + globals.guess[j].upper() + "  "
+                        except:
+                            b += "    "
+                        c += "└─┘ "
+                    stdscr.addstr("{0}\n{1}\n{2}\n".format(a, b, c), curses.color_pair(1))
+                else:
+                    stdscr.addstr("┌─┐ ┌─┐ ┌─┐ ┌─┐ ┌─┐\n\n└─┘ └─┘ └─┘ └─┘ └─┘\n", curses.color_pair(1))
 
-            elif globals.guess[0] != "" and globals.guess[1] == i:
-                a = ""
-                b = ""
-                c = ""
-                for j in range(5):
-                    a += "┌─┐ "
-                    try:
-                        b += " " + str(globals.guess[0][j]).upper() + "  "
-                    except:
-                        b += "    "
-                    c += "└─┘ "
-                stdscr.addstr("{0}\n{1}\n{2}\n".format(a, b, c), curses.color_pair(1))
-            else:
-                stdscr.addstr("┌─┐ ┌─┐ ┌─┐ ┌─┐ ┌─┐\n\n└─┘ └─┘ └─┘ └─┘ └─┘\n", curses.color_pair(1))
+        if globals.guesses:
+            pass
 
 parser = argparse.ArgumentParser(description="Wordle")
 parser.add_argument("--daily", help="syncs with the real wordle on new york times", action="store_true")
 parser.add_argument("--tries", help="number of tries", type=int, default=6)
 args = parser.parse_args()
-
-while len(globals.guesses) < args.tries: globals.guesses.append("")
 
 path = "en"
 f = open(path, "r")
@@ -124,7 +114,6 @@ else:
 
 globals.word = swords[num]
 swords = None
+print("the word was {0}".format(globals.word))
 
 curses.wrapper(renderModeCurses.main)
-
-
