f79c458825b08300dc78c73b2876bff23a65c6b0
decawas <t3d62no8@decawas.anonaddy.me>
2024-01-27 22:31:10 +0000

    changes

--- cursle.py
+++ cursle.py
@@ -1,13 +1,11 @@
-import argparse, curses, json, hashlib, math, random, re, time
+import argparse, curses, json, hashlib, random, time
 
 class Cursle:
 	def __init__(self):
 		try:
-			with open(f"lang/{args.length}/{args.lang}", "r") as f:
-				words = f.read()
+			words = open(f"lang/{args.length}/{args.lang}", "r").read()
 		except FileNotFoundError:
-			with open(f"lang/5/en", "r") as f:
-				words = f.read()
+			words = open(f"lang/5/en", "r").read()
 			args.length = 5
 
 		with open("lang.json", "r") as langdict:
@@ -18,17 +16,12 @@ class Cursle:
 		elif args.wordcode == -1:
 			args.wordcode = random.randint(0, 999999999)
 		word = hashlib.sha512()
-		word.update(bytes(math.floor(args.wordcode / 86400)))
-		if args.difficulty == "easy":
-			args.difficulty = int(len(words) / 2)
-		elif args.difficulty == "medium":
-			args.difficulty = int(len(words) / 2)
-		elif args.difficulty == "hard":
-			args.difficulty = int(len(words) / 2)
-		elif args.difficulty == "obscure":
-			args.difficulty = len(words)
-		elif type(args.difficulty) == "int" and args.diffculty > len(words):
-			args.diffculty = len(words)
+		word.update(bytes(args.wordcode // 86400))
+		try:
+			args.difficulty = {"easy": len(words.split("\n")) // 8, "medium": len(words.split("\n")) // 4, "hard": len(words.split("\n")) // 2, "obscure": len(words.split("\n"))}[args.difficulty]
+		except:
+			if type(args.difficulty) == "int" and args.difficulty > len(words).split("\n"):
+				args.diffculty = len(words)
 
 		word = [words.split("\n")[:args.difficulty][int(word.hexdigest(), 16) % len(words.split("\n")[:args.difficulty]) - 1]]
 
@@ -39,7 +32,7 @@ class Cursle:
 			offset = 16
 
 		if args.tries == -1:
-			args.tries = math.floor(len(alphab) / args.length) + 1
+			args.tries = len(alphab) // args.length + 1
 
 		self.word = word
 		self.words = words
@@ -61,33 +54,35 @@ def main(screen):
 
 	game = Cursle()
 	try:
-		render.renderinit(screen, game, math.floor(screen.getmaxyx()[1] / 2))
+		render.renderinit(screen, game, screen.getmaxyx()[1] // 2)
 	except curses.error:
 		screen.refresh()
 		screen.clear()
 		if args.length * 4 >= len(game.alphab): minx = args.length
 		else: minx = len(game.alphab)
 		screen.addstr(f"terminal too small\n must be >= {minx}x{args.tries * 3 + 2}")
-	x, y = 0, 0
 	while True:
 		if len(game.guesses) > 0 and game.guesses[-1] == game.word[0] or len(game.guesses) == args.tries:
 			end(screen, game)
 
 		key = str(screen.get_wch())
-		maxx = math.floor(screen.getmaxyx()[1] / 2)
+		halfx = screen.getmaxyx()[1] // 2
 
 		if key == "409":
-			_, x, y, _, _ = curses.getmouse()
+			try:
+				_, x, y, _, _ = curses.getmouse()
+			except curses.error:
+				x, y = 0, 0
 			if len(game.guess) < args.length and y == 3 * args.tries + 1:
-				try: 
-					game.guess += game.alphab[x - maxx - math.floor(len(game.alphab) / 2)]
-					render.guessing(screen, game)
+				try:
+					game.guess += game.alphab[x - halfx - len(game.alphab) // 2]
+					render.guessing(screen, game, halfx)
 				except IndexError:
 					pass
 			elif y == 3 * args.tries:
-				if x > maxx - 9 and x < maxx - 3:
+				if x > halfx - 9 and x < halfx - 3:
 					key = "\n"
-				elif x > maxx - 3 and x < maxx + 7:
+				elif x > halfx - 3 and x < halfx + 7:
 					key = "263"
 
 		if key == "1":
@@ -96,33 +91,32 @@ def main(screen):
 			key = ""
 		elif key == "410":
 			try: 
-				render.renderinit(screen, game, maxx)
+				render.renderinit(screen, game, halfx)
 				if len(game.guesses) != 0:
-					render.guessed(screen, game, maxx)
-				render.guessing(screen, game, maxx)
+					render.guessed(screen, game, halfx)
+				render.guessing(screen, game, halfx)
 			except curses.error: 
 				screen.refresh()
 				if args.length * 4 >= len(game.alphab): minx = args.length
 				else: minx = len(game.alphab)
 				screen.addstr(f"terminal too small\n must be >= {minx}x{args.tries * 3 + 2}")
-		elif key == "\n" and re.search(f"{game.guess}\n", game.words) != None and len(game.guess) == args.length:
+		elif key == "\n" and game.guess in game.words != None and len(game.guess) == args.length:
 			game.guesses.append(game.guess)
 			game.guess = ""
-			render.guessed(screen, game, maxx)
+			render.guessed(screen, game, halfx)
 		elif key == "263":
 			game.guess = game.guess[:-1]
-			render.guessing(screen, game, maxx)
-		elif len(game.guess) != args.length and len(key) == 1 and re.search(key, game.alphab) != None:
+			render.guessing(screen, game, halfx)
+		elif len(game.guess) != args.length and len(key) == 1 and key in game.alphab:
 			game.guess += key
-			render.guessing(screen, game, maxx)
+			render.guessing(screen, game, halfx)
 
 def end(screen, game):
 	if not args.daily:
 		mode = "free"
 	if args.daily:
 		mode = "daily"
-	with open(f"{mode}.stats", "r") as f:
-		stats = f.read().split(",")
+	stats = open(f"{mode}.stats", "r").read().split(",")
 
 	for i in range(len(stats)):
 		stats[i] = int(stats[i])
@@ -159,43 +153,43 @@ def end(screen, game):
 				screen.addstr(f"terminal too small\n must be >= {minx}x{args.tries * 3 + 2}")
 
 class render:
-	def renderinit(screen, game, maxx):
+	def renderinit(screen, game, halfx):
 		screen.refresh()
 		screen.clear()
 		for i in range(args.tries):
-			screen.addstr(i * 3, maxx - (args.length * 2 - 1), "╭─╮ "*args.length)
-			screen.addstr(i * 3 + 2, maxx - (args.length * 2 - 1), "╰─╯ "*args.length)
-		screen.addstr(3 * args.tries, maxx - 7, "ENTER BACKSPACE")
-		screen.addstr(3 * args.tries + 1, maxx - math.floor(len(game.alphab) / 2), game.alphab.upper())
+			screen.addstr(i * 3, halfx - (args.length * 2 - 1), "╭─╮ "*args.length)
+			screen.addstr(i * 3 + 2, halfx - (args.length * 2 - 1), "╰─╯ "*args.length)
+		screen.addstr(3 * args.tries, halfx - 7, "ENTER BACKSPACE")
+		screen.addstr(3 * args.tries + 1, halfx - len(game.alphab) // 2, game.alphab.upper())
 
-	def guessed(screen, game, maxx):
+	def guessed(screen, game, halfx):
 		for i in range(len(game.guesses)):
 			for j in range(args.length):
 				if game.word[0][j] == game.guesses[i][j]:
 					colour = curses.color_pair(2)
-					screen.addstr(i * 3 + 2, maxx - (args.length * 2 - 1) + (j * 4), "╰v╯", colour)
-				elif game.word[0][j] != game.guesses[i][j] and re.search(game.guesses[i][j], game.word[0]) != None:
+					screen.addstr(i * 3 + 2, halfx - (args.length * 2 - 1) + (j * 4), "╰v╯", colour)
+				elif game.word[0][j] != game.guesses[i][j] and game.guesses[i][j] in game.word[0]:
 					colour = curses.color_pair(3)
-					screen.addstr(i * 3 + 2, maxx - (args.length * 2 - 1) + (j * 4), "╰-╯", colour)
-				elif game.word[0][j] != game.guesses[i][j] and re.search(game.guesses[i][j], game.word[0]) == None:
+					screen.addstr(i * 3 + 2, halfx - (args.length * 2 - 1) + (j * 4), "╰-╯", colour)
+				elif game.word[0][j] != game.guesses[i][j] and game.guesses[i][j] not in game.word[0]:
 					colour = curses.color_pair(1)
-					screen.addstr(i * 3 + 2, maxx - (args.length * 2 - 1) + (j * 4), "╰x╯", colour)
-				screen.addstr(i * 3 + 1, maxx - (args.length * 2 - 1) + abs(j * 4 - game.offset), f"│{game.guesses[i][j].upper()}│", colour)
-				screen.addstr(i * 3, maxx - (args.length * 2 - 1) + (j * 4), "╭─╮", colour)
+					screen.addstr(i * 3 + 2, halfx - (args.length * 2 - 1) + (j * 4), "╰x╯", colour)
+				screen.addstr(i * 3 + 1, halfx - (args.length * 2 - 1) + abs(j * 4 - game.offset), f"│{game.guesses[i][j].upper()}│", colour)
+				screen.addstr(i * 3, halfx - (args.length * 2 - 1) + (j * 4), "╭─╮", colour)
 	
-	def guessing(screen, game, maxx):
+	def guessing(screen, game, halfx):
 		i = len(game.guesses)
-		screen.addstr(i * 3 + 1, abs(len(game.guess) * 4 - game.offset) + maxx - (args.length * 2 - 2), " ")
+		screen.addstr(i * 3 + 1, abs(len(game.guess) * 4 - game.offset) + halfx - (args.length * 2 - 2), " ")
 		for j in range(args.length):
 			try:
-				screen.addstr(i * 3 + 1, abs(j * 4 - game.offset) + maxx - (args.length * 2 - 2), game.guess[j].upper())
+				screen.addstr(i * 3 + 1, abs(j * 4 - game.offset) + halfx - (args.length * 2 - 2), game.guess[j].upper())
 			except:
 				break
 	
 	def end(screen, game, stats):
 		br = " ▏▎▍▌▋▊▉█─"
 		screen.clear()
-		maxx = math.floor(screen.getmaxyx()[1] / 2)
+		halfx = screen.getmaxyx()[1] // 2
 		maxs = stats[2:]
 		maxs.sort()
 		maxs = maxs[-1]
@@ -210,18 +204,18 @@ class render:
 			except ZeroDivisionError:
 				segments, subsegments = round(stats[i + 2] / 1 * (args.length * 4 - 3)), round((stats[i + 2] / 1 - int(stats[i + 2] / 1)) * 8)
 
-			screen.addstr(i * 3, maxx - (args.length * 2 - 1), f"╭{br[9]*(args.length * 4 - 3)}╮", colour)
-			screen.addstr(i * 3 + 1, maxx - (args.length * 2 - 1), f"│{br[0]*(args.length * 4 - 3)}│ {stats[i+2]}", colour)
+			screen.addstr(i * 3, halfx - (args.length * 2 - 1), f"╭{br[9]*(args.length * 4 - 3)}╮", colour)
+			screen.addstr(i * 3 + 1, halfx - (args.length * 2 - 1), f"│{br[0]*(args.length * 4 - 3)}│ {stats[i+2]}", colour)
 			if subsegments > 0:
-				screen.addstr(i * 3 + 1, maxx - (args.length * 2 - 2), f"{br[8]*segments}{br[subsegments]}", colour)
+				screen.addstr(i * 3 + 1, halfx - (args.length * 2 - 2), f"{br[8]*segments}{br[subsegments]}", colour)
 			else:
-				screen.addstr(i * 3 + 1, maxx - (args.length * 2 - 2), f"{br[8]*segments}", colour)
-			screen.addstr(i * 3 + 2, maxx - (args.length * 2 - 1), f"╰{br[9]*(args.length * 4 - 3)}╯", colour)
+				screen.addstr(i * 3 + 1, halfx - (args.length * 2 - 2), f"{br[8]*segments}", colour)
+			screen.addstr(i * 3 + 2, halfx - (args.length * 2 - 1), f"╰{br[9]*(args.length * 4 - 3)}╯", colour)
 
 		try:
-			screen.addstr(3 * args.tries, maxx - 7, f"Wins: {stats[0]}  Losses: {stats[1]}")
+			screen.addstr(3 * args.tries, halfx - 7, f"Wins: {stats[0]}  Losses: {stats[1]}")
 		except ZeroDivisionError:
-			screen.addstr(3 * args.tries, maxx - 9, f"Wins: {stats[0]}  Losses: {stats[1]}")
+			screen.addstr(3 * args.tries, halfx - 9, f"Wins: {stats[0]}  Losses: {stats[1]}")
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Cursle")
